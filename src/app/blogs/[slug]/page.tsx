3c34ebad92ec3156d2e844be95b330e186bb9db5
kabsmeiou <cvincentcabral@gmail.com>
2025-12-25 22:33:12 +0800

    add: blog post view added

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,12 +1,56 @@
 // /blog/[id] route
+"use client";
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import MarkdownRenderer from "@/components/MarkdownRenderer";
+
+import { Blog } from "@/components/BlogCard";
 
 export default function BlogPostPage() {
+    const id = useParams<{ id: string }>().slug;
+    const [content, setContent] = useState<string>("");
+    const [metadata, setMetadata] = useState<Blog | null>(null);
+
+    useEffect(() => {
+        const fetchBlog = async() =>  {
+            console.log("Fetching blog data for ID:", id);
+            try {
+                const response = await fetch(
+                    `https://raw.githubusercontent.com/kabsmeiou/kabsmeiou.github.io/refs/heads/main/content/blogs/${id}.md`
+                );
+                if (!response.ok) throw new Error("Failed to fetch blog data");
+                const contentData = await response.text();
+                setContent(contentData);
+            } catch (error) {
+                console.error("Failed to fetch blog data:", error);
+                return;
+            }
+            // fetch blog metadata
+            try {
+                const response = await fetch(
+                    `https://raw.githubusercontent.com/kabsmeiou/kabsmeiou.github.io/refs/heads/main/content/blogs.json`
+                );
+                if (!response.ok) throw new Error("Failed to fetch blog metadata");
+                const metadata: Blog[] = await response.json();
+                const blogMeta = metadata.find((blog) => blog.id === id) || null;
+                setMetadata(blogMeta);
+            } catch (error) {
+                console.error("Failed to fetch blog metadata:", error);
+                return;
+            }
+        };
+        if (id) fetchBlog();
+        else console.error("No blog ID provided in URL.");
+    }, [id]);
+
     return (
         <>
-            <h1 className="text-4xl font-bold text-black dark:text-zinc-50">Blog Post Page</h1>
-            <p className="text-zinc-700 dark:text-zinc-300 text-lg">
-                This is a placeholder for individual blog posts. Content will be added here in the future.
-            </p>
+            <section className="flex flex-col gap-y-8 py-16 px-4 sm:px-16 max-w-4xl mx-auto">
+                <h1 className="text-4xl font-semibold text-black dark:text-zinc-50 text-center">
+                    {metadata ? metadata.title : "Loading..."}
+                </h1>
+                <MarkdownRenderer content={content} />
+            </section>
         </>
     );
 }
